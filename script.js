a271c4bd35095a6b885861322c942a5986b7e68f
McCrony <askmccrony@gmail.com>
2025-05-30 08:46:18 +0000

    Target DOM Element with js

---
+++ script.js
@@ -0,0 +1,6 @@
+//DOM ELEMENTS
+
+const hourElement = document.querySelector('#hrs'),
+    minElement = document.querySelector('#min'),
+    secElement = document.querySelector('#secs'),
+    am_pmElement = document.querySelector('.am-pm');
