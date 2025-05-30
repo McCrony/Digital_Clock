1b423dd8b0f48e94310032304983c0b622110967
McCrony <askmccrony@gmail.com>
2025-05-30 08:41:18 +0000

    Clear exiting codes from js file

--- script.js
+++
@@ -1,9 +0,0 @@
-let seconds = document.querySelector('#seconds')
-const date = new Date();
-
-let hour = date.getHours();
-let secs = date.getSeconds();
-
-setInterval(() => {
-seconds.innerHTML = secs;
-}, 1000);
