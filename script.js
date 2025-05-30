e5436c22dc08a518ff2c7b0e0fc5775cbf371336
McCrony <askmccrony@gmail.com>
2025-05-30 09:08:54 +0000

    Create function to update and display time

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const hourElement = document.querySelector('#hrs'),
     minElement = document.querySelector('#min'),
     secElement = document.querySelector('#secs'),
     am_pmElement = document.querySelector('.am-pm');
+
+// A FUNCTION AND CALLING IT EVERY SECOND
+
+
+setInterval(()=>{
+    let date = new Date();
+    let hours = date.getHours(),
+        minutes = date.getMinutes(),
+        seconds = date.getSeconds();
+        
+        hourElement.textContent = hours;
+        minElement.textContent = minutes;
+        secElement.textContent = seconds;
+
+        if(hours < 12){
+            hourElement.textContent = `0${hours}`;
+        }
+    
+        if(minutes < 10){
+            minElement.textContent = `0${minutes}`;
+        }
+    
+        if(seconds < 10){
+            secElementElement.textContent = `0${seconds}`;
+        }
+}, 1000)
